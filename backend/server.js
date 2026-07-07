d59b9abe775025976b8579e01897e65f6fa0d413
moh <amjamal4050@gmail.com>
2026-07-07 22:14:09 +0300

    chore: setup frontend Vue with Tailwind, Axios and backend CORS

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 const express = require("express");
 require("dotenv").config();
-const authRouter = require("./src/route/authRoute")
-const budgetRouter = require("./src/route/budgetRoute")
+const authRouter = require("./src/route/authRoute");
+const budgetRouter = require("./src/route/budgetRoute");
 const errorHandler = require("./src/middleware/error");
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 
-app.use("/",authRouter);
-app.use("/api",budgetRouter);
+app.use("/", authRouter);
+app.use("/api", budgetRouter);
 
 app.use(errorHandler);
 
---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1></h1>
+  <p>Hello world</p>
+</template>
+
+<style scoped></style>
